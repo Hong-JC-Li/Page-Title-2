73afd44f46c831536dda97216eda5035dd0b7e16
honglihongli <44278904+honglihongli@users.noreply.github.com>
2019-03-14 16:35:44 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,27 @@
+$(".button").click(function() {
+    var project2 = $(".eek").val();
+    var project = $(".kee").val();
+    console.log (project);
+    console.log(parseInt(project2));
+    console.log (project2);
+    console.log(parseInt(project2));
+    $(".cup").text("Chungus will eat ");
+    $(".cup").append((3000-(project))*(project2) + " carrots by the year 3000.");
+    $(".whatthe").css("display","none");
+});
+$(".project").click(function() {
+    $(".whatthe").text("Now fill out the above and then click that submit button to find out how many carrots Big Chungus will eat by 3000!");
+});
+$(".button2") .click(function() {
+    var thirdthird = $(".tahw").val();
+    $(".theresnoway").text("I guess they never ");
+    $(".theresnoway").append(thirdthird + ", huh?");
+    $(".MEMES2, .one, .tahw1, .button21, .theresnoway1").show();
+});
+$(".button21") .click(function() {
+    var what = $(".tahw1").val();
+    console.log (what);
+    console.log(parseInt(what));
+    $(".theresnoway1").text("In 3000 years, a computer with your determination will have typed into this button ");
+    $(".theresnoway1").append((what*1095000)+" times.");
+});
